fe06f0649a1c6f2bfa6bf67ae166bf0a7ab24958
ManbirKaur <70414362+ManbirKaur@users.noreply.github.com>
2023-07-14 15:25:30 -0700

    first commit

---
+++ variable_learning.py
@@ -0,0 +1,15 @@
+"""
+There are three types of variables in python
+String which is enclosed in quotation marks ("") EX: str = "word"
+Numbers which can be whole or decimal EX: num1 = 50; num2 = 50.678910
+Boolean which is either true or false EX: is_bool = True; is_bool = False
+(NOTE: first letter must be capital for 'True' and 'False')
+"""
+
+character_name = "John"
+character_age = "99"
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+
+
