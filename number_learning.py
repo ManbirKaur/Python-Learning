42f2d10fc88ce4afff5edd1e53047523c4f22b70
ManbirKaur <70414362+ManbirKaur@users.noreply.github.com>
2023-07-27 21:12:48 -0700

    first commit

---
+++ number_learning.py
@@ -0,0 +1,15 @@
+from math import *                              #imports everything from math library
+print(7.5 * (3 + 5))                            #math operations can be performed in print statements
+                                                #BEDMAS rules apply; operators are (), **, /, *, +, -
+my_num = -13
+print(my_num)                                   #prints variable
+print(str(my_num) + " is my favourite number")  #converts number to string; helpful when printing num with str
+print(abs(my_num))                              #prints absolute value (positive number)
+print(pow(2, 3))                                #pow(x, y) prints the result of x to the power of y
+                                                #works same as  x**y
+print(max(55, 66))                              #prints bigger number
+print(min(55, 66))                              #prints smaller number
+print(round(3.75))                              #rounds number according to standard rounding rules
+print(floor(3.75))                              #rounds number down; imported from math library
+print(ceil(3.1))                                #rounds number up; imported from math library
+print(sqrt(36))                                 #prints square root of number; imported from math library
