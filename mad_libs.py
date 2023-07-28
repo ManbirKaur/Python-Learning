20ddb3d6dc6214e40efa6e3793fe20b41bd24638
ManbirKaur <70414362+ManbirKaur@users.noreply.github.com>
2023-07-27 22:14:26 -0700

    first commit

---
+++ mad_libs.py
@@ -0,0 +1,10 @@
+noun1 = input("Please enter a noun: ")
+name = input("Please enter a name: ")
+noun2 = input("Please enter a plural noun: ")
+verb = input("Please enter a past tense verb: ")
+print("Once upon a time there was a " + noun1)
+print("This " + noun1 + "'s name was " + name)
+print(name + " liked eating " + noun2)
+print("One day " + name + " ate too many " + noun2 + " and they " + verb)
+print("The end!")
+
